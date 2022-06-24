dd4a9a45ae2a800b722e5a6c2798d0b51d9e5de8
김하영 <khy4018@gmail.com>
2022-06-24 18:29:41 +0900

    npm 재설치, 스크립트 재수정

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,6 @@
 import { sayHello } from './util';
 import '../css/style.scss';
 
-function addImage() {
-    const img = document.createElement('img');
-    img.arc = "webpack";
-
-    const body = document.querySelector('body');
-    body.appendChild(img);
-}
 
 function myFunc() {
     sayHello('jack');
